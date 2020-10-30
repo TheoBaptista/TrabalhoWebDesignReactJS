2d4ebed502b64eeda605238875677b97ab75af70
TheoAlfonso <theoalfonso78@gmail.com>
2020-10-30 16:39:08 -0300

    Commit de todo projeto em react js

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/global.css';
+import Routes from './routes';
+
+
+
+
+//Type-JSX javasript xml
+function App() {
+  return (
+   <Routes />
+  );
+}
+
+export default App;
---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi'
+
+import {Map,TileLayer} from 'react-leaflet';
+
+import '../styles/pages/orphanages-map.css';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMakersImg from '../images/Map-marker.svg';
+
+function OrphanagesMap(){
+    return(
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMakersImg} alt="Happy"/>
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>
+                        Porto Alegre
+                    </strong>
+                    <span>Rio Grande do Sul</span>
+                </footer>
+            </aside>
+            
+            <Map
+             center={[-30.0406915,-51.2502248]}
+             zoom={12}
+             style={{width:'100%', height:'100%'}}>
+                 <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+             </Map>
+            
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
+    )
+}
+
+export default OrphanagesMap;
